79b15d6306dc10478e23cbcef3dca5fada899d17
GaoMatrix <gcquan08@gmail.com>
2015-03-12 07:17:21 +0800

    输入框的抖动和振动效果

--- src/com/gao/mobilesafe/NumberAddressQueryActivity.java
+++ src/com/gao/mobilesafe/NumberAddressQueryActivity.java
@@ -3,25 +3,34 @@ package com.gao.mobilesafe;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.gao.mobilesafe.db.dao.NumberAddressQueryUtils;
 
 public class NumberAddressQueryActivity extends Activity {
     private EditText mPhoneNumEditText;
     private TextView mResultTextView;
-
+    /**
+     * 系统提供的振动服务
+     */
+    private Vibrator vibrator;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_number_address_query);
         mPhoneNumEditText = (EditText) findViewById(R.id.ed_phone);
         mResultTextView = (TextView) findViewById(R.id.result);
+        vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
         mPhoneNumEditText.addTextChangedListener(new TextWatcher() {
 
             /**
@@ -65,9 +74,28 @@ public class NumberAddressQueryActivity extends Activity {
      */
     public void numberAddressQuery(View view) {
         String phone = mPhoneNumEditText.getText().toString().trim();
-        if (!TextUtils.isEmpty(phone)) {
+        if (TextUtils.isEmpty(phone)) {
+            Toast.makeText(this, "号码为空", 0).show();
+            Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+            mPhoneNumEditText.startAnimation(shake);
+
+            // 当电话号码为空的时候，就去振动手机提醒用户
+            // vibrator.vibrate(2000);
+            long[] pattern = {
+                    200, 200, 300, 300, 1000, 2000
+            };
+            // -1不重复 0循环振动 1；
+            vibrator.vibrate(pattern, -1);
+
+            return;
+        } else {
             String address = NumberAddressQueryUtils.queryNumber(phone);
             mResultTextView.setText(address);
+            // 去数据库查询号码归属地
+            // 1.网络查询 ；2.本地的数据库--数据库
+            // 写一个工具类，去查询数据库
+            Log.i("NumberAddressQueryActivity", "您要查询的电话号码==" + phone);
         }
+
     }
 }
