bb4bd260fdf0d966cda2e20877971404e334aacb
GaoMatrix <gcquan08@gmail.com>
2015-03-12 07:00:07 +0800

    号码归属地查询效果的优化

--- src/com/gao/mobilesafe/NumberAddressQueryActivity.java
+++ src/com/gao/mobilesafe/NumberAddressQueryActivity.java
@@ -3,7 +3,9 @@ package com.gao.mobilesafe;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -20,6 +22,40 @@ public class NumberAddressQueryActivity extends Activity {
         setContentView(R.layout.activity_number_address_query);
         mPhoneNumEditText = (EditText) findViewById(R.id.ed_phone);
         mResultTextView = (TextView) findViewById(R.id.result);
+        mPhoneNumEditText.addTextChangedListener(new TextWatcher() {
+
+            /**
+             * 当文本发生变化的时候回调
+             */
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s != null && s.length() >= 3) {
+                    // 查询数据库，并且显示结果
+                    String address = NumberAddressQueryUtils.queryNumber(s.toString());
+                    mResultTextView.setText(address);
+                }
+
+            }
+
+            /**
+             * 当文本发生变化之前回调
+             */
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                    int after) {
+                // TODO Auto-generated method stub
+
+            }
+
+            /**
+             * 当文本发生变化之后回调
+             */
+            @Override
+            public void afterTextChanged(Editable s) {
+                // TODO Auto-generated method stub
+
+            }
+        });
     }
 
     /**
