bf5d3977cc3fbd706774902698612c3fbca8bf17
GaoMatrix <gcquan08@gmail.com>
2015-03-11 08:45:06 +0800

    读取联系人,设置安全号码

--- src/com/gao/mobilesafe/Setup3Activity.java
+++ src/com/gao/mobilesafe/Setup3Activity.java
@@ -4,13 +4,17 @@ package com.gao.mobilesafe;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class Setup3Activity extends BaseSetupActivity {
+    private EditText et_setup3_phone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup3);
+        et_setup3_phone = (EditText) findViewById(R.id.et_setup3_phone);
+        et_setup3_phone.setText(mSharedPreferences.getString("safenumber", ""));
     }
 
     @Override
@@ -22,11 +26,34 @@ public class Setup3Activity extends BaseSetupActivity {
         overridePendingTransition(R.anim.tran_in, R.anim.tran_out);
     }
 
+    /**
+     * 选择联系人的点击事件
+     * 
+     * @param view
+     */
+    public void selectContact(View view) {
+        Intent intent = new Intent(this, SelectContactActivity.class);
+        startActivityForResult(intent, 0);
+
+    }
+
     @Override
     protected void showPre() {
         Intent intent = new Intent(this, Setup2Activity.class);
         startActivity(intent);
         finish();
         overridePendingTransition(R.anim.tran_pre_in, R.anim.tran_pre_out);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data == null)
+            return;
+
+        String phone = data.getStringExtra("phone").replace("-", "");
+        et_setup3_phone.setText(phone);
+
     }
 }
