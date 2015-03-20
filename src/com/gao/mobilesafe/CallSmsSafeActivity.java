aead672959d78700c3b9faa3a20277e707f44d8b
chengquan_gao <gaocq@cienet.com.cn>
2015-03-20 11:12:28 +0800

    Listview的优化

--- src/com/gao/mobilesafe/CallSmsSafeActivity.java
+++ src/com/gao/mobilesafe/CallSmsSafeActivity.java
@@ -31,19 +31,32 @@ public class CallSmsSafeActivity extends Activity {
     
     private class CallSmsSafeAdapter extends BaseAdapter {
 
+        //有多少条目被显示，这个方法就会被调用多少次。
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            View view = View.inflate(CallSmsSafeActivity.this, R.layout.list_item_callsms, null);
-            TextView tv_black_number = (TextView) view.findViewById(R.id.tv_black_number);
-            TextView tv_black_mode = (TextView) view.findViewById(R.id.tv_black_mode);
-            tv_black_number.setText(mInfos.get(position).getNumber());
+            View view;
+            ViewHolder holder;
+            // 1.减少内存中view对象的创建个数。
+            if (null == convertView) {
+                view = View.inflate(CallSmsSafeActivity.this, R.layout.list_item_callsms, null);
+                // 2.减少字孩子查询的次数，因为每次查询findViewById都需要遍历整个控件树。
+                holder = new ViewHolder();
+                holder.tv_number = (TextView) view.findViewById(R.id.tv_black_number);
+                holder.tv_mode = (TextView) view.findViewById(R.id.tv_black_mode);
+                // 当孩子生出来的时候找到他们的引用，存放到记事本，放到父亲的口袋。
+                view.setTag(holder);
+            } else {//复用listview缓存的对象
+                view = convertView;
+                holder = (ViewHolder) view.getTag();
+            }
+            holder.tv_number.setText(mInfos.get(position).getNumber());
             String mode = mInfos.get(position).getMode();
             if ("1".equals(mode)) {
-                tv_black_mode.setText("电话拦截");
+                holder.tv_mode.setText("电话拦截");
             } else if ("2".equals(mode)) {
-                tv_black_mode.setText("短信拦截");
+                holder.tv_mode.setText("短信拦截");
             } else {
-                tv_black_mode.setText("全部拦截");
+                holder.tv_mode.setText("全部拦截");
             }
             return view;
         }
@@ -65,6 +78,15 @@ public class CallSmsSafeActivity extends Activity {
             return 0;
         }
 
-        
+    }
+
+    /**
+     * view对象的容器，记录孩子的地址，相当于一个记事本
+     * @author gaochengquan
+     *
+     */
+    static class ViewHolder {
+        TextView tv_number;
+        TextView tv_mode;
     }
 }
