fb4e429a11d42252bccccea65766b61c84402cc9
chengquan_gao <gaocq@cienet.com.cn>
2015-03-23 10:56:49 +0800

    电话拦截

--- src/com/gao/mobilesafe/service/CallSmsSafeService.java
+++ src/com/gao/mobilesafe/service/CallSmsSafeService.java
@@ -1,21 +1,28 @@
 
 package com.gao.mobilesafe.service;
 
+import java.lang.reflect.Method;
+
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.IBinder;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
 import android.telephony.gsm.SmsMessage;
 import android.util.Log;
 
+import com.android.internal.telephony.ITelephony;
 import com.gao.mobilesafe.db.dao.BlackNumberDao;
 
 public class CallSmsSafeService extends Service {
     public static final String TAG = CallSmsSafeService.class.getSimpleName();
     private InnerSmsReceiver mReceiver;
     private BlackNumberDao mDao;
+    private TelephonyManager mTelephonyManager;
+    private MyListener mListener;
 
     @Override
     public IBinder onBind(Intent arg0) {
@@ -53,16 +60,60 @@ public class CallSmsSafeService extends Service {
     public void onCreate() {
         mDao = new BlackNumberDao(this);
         mReceiver = new InnerSmsReceiver();
+        mTelephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        mListener = new MyListener();
+        mTelephonyManager.listen(mListener, PhoneStateListener.LISTEN_CALL_STATE);
         IntentFilter filter = new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
         filter.setPriority(IntentFilter.SYSTEM_HIGH_PRIORITY);
         registerReceiver(mReceiver, filter);
         super.onCreate();
+    }
+    
+    private class MyListener extends PhoneStateListener {
+        @Override
+        public void onCallStateChanged(int state, String incomingNumber) {
+            switch (state) {
+                case TelephonyManager.CALL_STATE_RINGING://响铃状态
+                    String result = mDao.findMode(incomingNumber);
+                    if("1".equals(result) || "3".equals(result) ){
+                        Log.d(TAG, "挂断电话....");
+                        endCall();
+                    }
+                    
+                    break;
+
+                default:
+                    break;
+            }
+            super.onCallStateChanged(state, incomingNumber);
+        }
     }
 
     @Override
     public void onDestroy() {
         unregisterReceiver(mReceiver);
         mReceiver = null;
+        mTelephonyManager.listen(mListener, PhoneStateListener.LISTEN_NONE);
         super.onDestroy();
+    }
+
+    /**
+     *查看源码来写的
+     */
+    public void endCall() {
+        //IBinder iBinder = ServiceManager.getService(TELEPHONY_SERVICE);
+        //ServiceManager是隐藏的类，需要通过反射来得到
+        //加载ServiceManager的字节码
+        Class clazz;
+        try {
+            clazz = CallSmsSafeService.class.getClassLoader().loadClass("android.os.ServiceManager");
+            Method method = clazz.getDeclaredMethod("getService", String.class);
+            IBinder iBinder = (IBinder) method.invoke(null, TELEPHONY_SERVICE);
+            ITelephony.Stub.asInterface(iBinder).endCall();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
     }
 }
